1884b6741d4621be7d69373e4adf86b1d8787bb2
Craig Blake <craigwblake@mac.com>
2010-10-11 22:05:33 -0400

    Deploys locally.

--- project/build/ScalaParallelProject.scala
+++ project/build/ScalaParallelProject.scala
@@ -2,6 +2,9 @@ import sbt._
 
 class ScalaParallelProject(info: ProjectInfo) extends DefaultProject(info) with AutoCompilerPlugins {
 
+	override def managedStyle = ManagedStyle.Maven
+	lazy val publishTo = "Local" at "file://"+Path.userHome+"/.m2/repository"
+
   val st = "org.scalatest" % "scalatest" % "1.0-for-scala-2.8.0-SNAPSHOT" % "test->default"
 
   val snapshotRepo = "Snapshot Repository" at "http://www.scala-tools.org/repo-snapshots"
